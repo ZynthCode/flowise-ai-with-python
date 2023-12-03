2eff4288b3e7499ac4bdebb928976f88d269ce02
Zynth <146667056+ZynthCode@users.noreply.github.com>
2023-12-03 15:07:01 +0100

    WIP

--- ai.py
+++ ai.py
@@ -8,36 +8,39 @@ from file_util import FileUtil
 
 from session_manager import SessionManager
 
-API_URL = "http://localhost:3000/api/v1/prediction/dc866a6c-41bb-49b4-aa0f-e4c0adbecd53" # API pointing to our locally running flowise API
+# API_URL = "http://localhost:3000/api/v1/prediction/dc866a6c-41bb-49b4-aa0f-e4c0adbecd53" # API pointing to our locally running flowise API
+API_URL = "http://localhost:3000/api/v1/prediction/fd62cfc9-a1b7-479b-acb4-712ac6a01f3a"
 
 def query(payload):
     response = requests.post(API_URL, json=payload)
     return response.json()
 
 def main():
-    if len(sys.argv) < 2:
-        print("Please provide a question!")
-        return
-
-    question = ' '.join(sys.argv[1:])
-    # TODO: Simulate output without just the text
-    # output = query({"question": question})
-    # answer = output["text"]
-
-    # testing part
-    answer = FileUtil.read_file("test/ai_markdown_answer.md")
-
-    better_answer = Markdown(answer)
-    print("\n")
-    print(better_answer)
-    print()
-    pyperclip.copy(answer)
-    print("[bold]Raw answer copied to clipboard.[/bold]")
-
     session_manager = SessionManager("logs/conversation_history.json")
-    # session_id = str(uuid.uuid4())
-    session_id = "36f05135-e259-4a4d-8f7a-afc78bec3c16"
-    session_manager.create_session(session_id, question, answer)
+    chat_id = str(uuid.uuid4()) 
+    history = []
+
+    print("Welcome! Ask me anything. Type 'exit' to quit.")
+    while True:
+        question = input("Your question: ")
+        if question.lower() == 'exit':
+            print("Goodbye!")
+            break
+
+        output = query({"question": question, "history": history})
+        answer = output["text"]
+
+        formatted_answer = Markdown(answer)
+        print("\n")
+        print(formatted_answer)
+        print()
+        pyperclip.copy(answer)
+        print("[bold]Raw answer copied to clipboard.[/bold]")
+
+        history.append({"type": "userMessage", "message": question})
+        history.append({"type": "apiMessage", "message": answer})
+
+        session_manager.create_chat(chat_id, history) 
 
 
 if __name__ == "__main__":
--- session_manager.py
+++ session_manager.py
@@ -6,29 +6,36 @@ class SessionManager:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
-    def create_session(self, session_id: str, question: str, answer: str):
+    def create_chat(self, chat_id: str, history: list):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         data = self._read_file()
-        if session_id not in data:
-            data[session_id] = []
-        data[session_id].append({"timestamp": timestamp, "question": question, "answer": answer})
+        if chat_id not in data:
+            data[chat_id] = []
+        
+        # Add only new conversations
+        for msg in history:
+            if msg not in data[chat_id]: 
+                msg_with_timestamp = {"timestamp": timestamp, **msg}
+                data[chat_id].append(msg_with_timestamp)
+        
         self._write_file(data)
 
-    def get_session(self, session_id: str) -> List[Dict[str, str]]:
+    def get_chat(self, chat_id: str) -> List[Dict[str, str]]:
         data = self._read_file()
-        return data.get(session_id, [])
+        return data.get(chat_id, [])
 
-    def update_session(self, session_id: str, question: str, answer: str):
+    def update_chat(self, chat_id: str, question: str, answer: str):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         data = self._read_file()
-        if session_id in data:
-            data[session_id].append({"timestamp": timestamp, "question": question, "answer": answer})
+        if chat_id in data:
+            data[chat_id].append({"timestamp": timestamp, "type": "userMessage", "message": question})
+            data[chat_id].append({"timestamp": timestamp, "type": "apiMessage", "message": answer})
             self._write_file(data)
 
-    def delete_session(self, session_id: str):
+    def delete_chat(self, chat_id: str):
         data = self._read_file()
-        if session_id in data:
-            del data[session_id]
+        if chat_id in data:
+            del data[chat_id]
             self._write_file(data)
 
     def _read_file(self) -> Dict[str, List[Dict[str, str]]]:
